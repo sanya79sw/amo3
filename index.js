20628376a55e7cdd06f80e69caf0286f97be8631
Alex <sanya79sw@gmail.com>
2019-04-22 20:14:26 +0300

    frontend

--- index.js
+++ index.js
@@ -1,43 +1,47 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const MongoClient = require("mongodb").MongoClient;
 const bcrypt = require('bcrypt');
+const MongoClient = require('mongodb').MongoClient;
 
+const app = express();
 
-var urlencodedParser = bodyParser.urlencoded({
-    extended: false
-});
-
-var app = express();
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.set('view engine', 'ejs');
 
 app.use('/public', express.static('public'));
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.post('/', urlencodedParser, function (req, res) {
-    var url = "mongodb://localhost:27017";
+app.get('/welcome', (req, res) => {
+    res.render('welcome');
+});
+
+app.post('/', urlencodedParser, (req, res) => {
+
+    const url = 'mongodb://localhost:27017';
 
     MongoClient.connect(url, async function (err, client) {
-        if (err) throw err;
+        if(err) throw err;
+    
+        const db = client.db('login');
+        const users = await db.collection('data').find({ user: req.body.user }).toArray();
 
-        var db = client.db('login');
-
-        //MongoDB
-        const users = await db.collection('data').find({user: req.body.user}).toArray();
-        if (users.length) {
-            const [user] = users;
-            const isValid = await bcrypt.compare(req.body.pass, user.pass);
-            
-            
-        } else {
-            res.end('FAIL');
+        if(users.length) {
+            let [user] = users;
+            bcrypt.compare(req.body.pass, user.pass, (err, bool) => {
+                if(err) throw err;
+                if(bool) {
+                    res.redirect('/welcome');
+                }
+            });
         }
+        client.close();
     });
-    res.render('index');
 });
+
+
 
 app.listen(3000);
